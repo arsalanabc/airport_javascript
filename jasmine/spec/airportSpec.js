7eaaa0c2c2b98038ccdfb80ca96373427778903a
arsalanabc <arsalanabc@gmail.com>
2018-06-12 17:25:19 +0100

    no takeoff if weather is stormy

--- jasmine/spec/airportSpec.js
+++ jasmine/spec/airportSpec.js
@@ -1,10 +1,34 @@
+
 describe('Airport', function() {
   it ('confirms the plane is no longer at the airport', function () {
     plane = new Plane();
     airport = new Airport();
+    
+    //airport.stormyWeather();
+
     airport.lands(plane);
-    expect(airport.planes).toContain(plane);
+
+    expect(airport._planes).toContain(plane); // illegal
+    
+    spyOn(airport,'stormyWeather').and.returnValue(false);
+
+ console.log(airport);
+
     airport.takesOff(plane);
-    expect(airport.planes).not.toContain(plane);
+    expect(airport._planes).not.toContain(plane);
   });
+
+  it ('no take offs in stormy weather', function () {
+  	airport = new Airport();
+
+  	 console.log(airport);
+  	 spyOn(airport,'stormyWeather').and.returnValue(true);
+
+   	//expect(airport.takesOff(new Plane)).toThrow(new Error("Stromy weather"));
+   	expect(function(){airport.takesOff(new Plane);}).toThrow(new Error ("Stormy weather"));
+
+   	//expect( function(){ throw new Error("Parsing is not possible") } ).toThrow(new Error("Parsing is not possible"));
+  });
+
+
 });
--- jasmine/src/Airport.js
+++ jasmine/src/Airport.js
@@ -1,11 +1,29 @@
 function Airport() {
-  this.planes = [];
+  this._planes = [];
 };
 
 Airport.prototype.lands = function(plane) {
-	this.planes.push(plane);
+	this._planes.push(plane);
 };
 
-Airport.prototype.takesOff = function(plane) {
-	this.planes.pop();
+
+
+Airport.prototype.stormyWeather = function(){
+ 	
+ 	if (Math.floor(Math.random()*10) > 7)
+ 		{return true;}
+ 	else {return false;}
+ 
+
 };
+
+
+Airport.prototype.takesOff = function(plane) {
+
+	if (this.stormyWeather()){
+		throw new Error("Stormy weather");
+	}
+	this._planes.pop();
+};
+
+
